c5c02ff7acb9ac3972ea6cd94fb9b480cafe8e91
Arpit Rai <arpitr2002@outlook.com>
2023-12-14 01:17:44 +0530

    Pattern

---
+++ Pattern/FullPyramid.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++){
+        for(int colspace=0;colspace<n)
+    }
+}
---
+++ Pattern/InvertedNumericHalfPyramid.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int row=n;row>0;row--){
+        for(int col=1;col<=row;col++){
+            cout<<col;
+        }
+        cout<<endl;
+    }
+}
