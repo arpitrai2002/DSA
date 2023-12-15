d2544c02244940d3512a961923400e5859480351
Arpit Rai <arpitr2002@outlook.com>
2023-12-15 19:47:17 +0530

    Pattern

--- Pattern/HollowDiamond.cpp
+++ Pattern/HollowDiamond.cpp
@@ -6,6 +6,31 @@ int main(){
     int rowcounter;
     cin>>rowcounter;
     for(int row=0;row<rowcounter;row++){
-        for(int colspace)
+        for(int colspace=0;colspace<rowcounter-row-1;colspace++){
+            cout<<"  ";
+        }
+        for(int col=0;col<row+1;col++){
+            if((col==0)||(col==row)){
+                cout<<" *  ";
+            }
+            else{
+                cout<<"    ";
+            }
+        }
+        cout<<endl;
+    }
+    for(int row=rowcounter-1;row>=0;row--){
+        for(int colspace=0;colspace<rowcounter-row-1;colspace++){
+            cout<<"  ";
+        }
+        for(int col=0;col<row+1;col++){
+            if((col==0)||(col==row)){
+                cout<<" *  ";
+            }
+            else{
+                cout<<"    ";
+            }
+        }
+        cout<<endl;
     }
 }
