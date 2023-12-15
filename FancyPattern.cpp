f61c45718e47b996a1d7fbe3e52a9d48f54a62c9
Arpit Rai <arpitr2002@outlook.com>
2023-12-15 20:30:43 +0530

    Pattern

---
+++ FancyPattern.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rowcounter;
+    cin>>rowcounter;
+
+    for(int row=1;row<=rowcounter;row++){
+        for(int col=1;col<=row;col++){
+            cout<<col;
+            if(col!=row){
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+    for(int row=rowcounter;row>0;row--){
+        for(int col=1;col<=row;col++){
+            cout<<col;
+            if(col!=row){
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+}
