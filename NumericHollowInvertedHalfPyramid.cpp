b795b1da2f441afaa4b93ca6d31ee4c5fc025f02
Arpit Rai <arpitr2002@outlook.com>
2023-12-16 12:47:44 +0530

    Pattern

---
+++ NumericHollowInvertedHalfPyramid.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rowcounter;
+    cin >> rowcounter;
+
+    for (int row = rowcounter; row > 0; row--)
+    {
+        for (int col = 1; col < row + 1; col++)
+        {
+            if ((row == 1) || (row == rowcounter))
+            {
+                cout <<col<<" ";
+            }
+            else
+            {
+                if ((col == 1) || (col == row))
+                {
+                    cout <<col<<" ";
+                }
+                else
+                {
+                    cout <<"  ";
+                }
+            }
+        }
+        cout << endl;
+    }
+}
