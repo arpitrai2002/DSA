e843dcda0b567b6a6d8c8a6f18ceaceb999531a3
Arpit Rai <arpitr2002@outlook.com>
2024-01-04 22:16:37 +0530

    ReverseAnArray

---
+++ NormalProblem.cpp/ReverseAnArray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int reverseAnArray(vector<int> &a){
+    int i=0;
+    int j=a.size()-1;
+    while(i<j){
+        swap(a[i],a[j]);
+        i++;
+        j--;
+    }
+}
+ 
+int main(){
+    vector<int> a{1,2,3,4,5,6,7,8,9};
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    reverseAnArray(a);
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+ return 0;
+}
