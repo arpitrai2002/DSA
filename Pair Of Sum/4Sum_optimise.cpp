2820a521bb45200ecb7a22eed170e04c9e83498b
Arpit Rai <arpitr2002@outlook.com>
2024-01-10 23:30:29 +0530

    4sum optimise

---
+++ Pair Of Sum/4Sum_optimise.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<vector<int>> FourSum(vector<int> a,int target){
+	vector<vector<int>> ans;
+	sort(a.begin(),a.end());
+	
+
+}
+ 
+int main(){
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
