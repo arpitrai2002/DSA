2176c5c6baad48fe148dc3b2c47cf993cad26ce3
Arpit Rai <arpitr2002@outlook.com>
2024-01-22 01:23:53 +0530

    Find Duplicate Number

---
+++ NormalProblem.cpp/FindDuplicateNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int Duplicate(vector<int> arr){
+    while(arr[0]!=arr[arr[0]]){
+        swap(arr[0],arr[arr[0]]);
+    }
+    return arr[0];
+}
+ 
+int main(){
+    vector<int> arr{1,3,4,2,4};
+    cout<<Duplicate(arr)<<endl;
+ 
+ 
+ return 0;
+}
