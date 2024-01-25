881e87990659548e5a4a2a31525df64bb8d1d2a0
Arpit Rai <arpitr2002@outlook.com>
2024-01-26 01:35:42 +0530

    Spiral Matrix II

---
+++ 2DArray/SpiralMatrix_II.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> Spiral(int n){
+    int row=n;
+    int col=n;
+    int total=n*n;
+
+    int start_row=0;
+    int end_row=row-1;
+    int start_col=0;
+    int end_col=col-1;
+
+    int count=0;
+
+    vector<vector<int>> ans(n,vector<int>(n,0));
+
+    while(count<total){
+        for(int i=start_col;i<=end_col && count<total;i++){
+            ans[start_row][i]=count+1;
+            count++;
+        }
+        start_row++;
+
+        for(int i=start_row;i<=end_row && count<total;i++){
+            ans[i][end_col]=count+1;
+            count++;
+        }
+        end_col--;
+
+        for(int i=end_col;i>=start_col && count<total;i--){
+            ans[end_row][i]=count+1;
+            count++;
+        }
+        end_row--;
+
+        for(int i=end_row;i>=start_row && count<total;i--){
+            ans[i][start_col]=count+1;
+            count++;
+        }
+        start_col++;
+
+    }
+    return ans;
+
+}
+
+ 
+int main(){
+    int n=2;
+    vector<vector<int>> ans=Spiral(n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+ 
+ 
+ 
+ return 0;
+}
