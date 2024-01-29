f5750e27f41b708584d540c2cc4a573eb01d796c
Arpit Rai <arpitr2002@outlook.com>
2024-01-29 21:13:06 +0530

    First Repeating Array

---
+++ NormalProblem.cpp/First_Repeating_Element.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+int firstRepeatingElement(vector<int> arr){
+    unordered_map<int,int> ans;
+    int n=arr.size();
+
+    for(int i=0;i<n;i++){
+        ans[arr[i]]++;
+    }
+
+    for(int i=0;i<n;i++){
+        if(ans[arr[i]]>1){
+            return i+1;
+        }
+    }
+    return -1;
+}
+ 
+int main(){
+    vector<int> arr{1,7,2,5,3,5};
+    cout<<firstRepeatingElement(arr)<<endl;
+ 
+ 
+ 
+ 
+ return 0;
+}
