e7cd7202b72903c031a3c37497d42fe3f828a0e8
Arpit Rai <arpitr2002@outlook.com>
2024-02-12 00:17:10 +0530

    char

---
+++ Char & String/Ch1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    char name[6];
+    cin>>name;
+    for(int i=0;i<6;i++){
+        cout<<name[i]<<endl;
+    }
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
