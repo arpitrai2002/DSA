30f88d035fa75b77288c4928a40ccbaa01ae8586
Arpit Rai <arpitr2002@outlook.com>
2024-02-12 09:52:06 +0530

    length

---
+++ Char & String/LengthOfString.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+ 
+int main(){
+    char name[100];
+    cin>>name;
+    int i=0;
+    while(name[i]!='\0'){
+        i++;
+
+    }
+    cout<<i<<endl;
+    cout<<strlen(name)<<endl;
+    
+ 
+ return 0;
+}
