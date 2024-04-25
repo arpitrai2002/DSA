e6cb6dc3b1df7facd38dcfdc4cd3d2fa553a870d
Arpit Rai <arpitr2002@outlook.com>
2024-04-25 21:13:11 +0530

    pointer

--- vector/array.cpp
+++ vector/array.cpp
@@ -3,9 +3,11 @@ using namespace std;
  
 int main(){
  int arr[10]={1,2,3,4};
- 
- 
- 
- 
+ cout<<arr<<endl;
+ cout<<&arr[0]<<endl;
+ cout<<&arr<<endl;
+
+ cout<<*arr+5<<endl;
+ cout<<*(arr+3)<<endl;
  return 0;
 }
