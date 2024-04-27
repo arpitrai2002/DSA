60942433caa5be0c87233fc2ecce580424cedb0e
Arpit Rai <arpitr2002@outlook.com>
2024-04-28 03:01:34 +0530

    reference Variable

---
+++ Reference Variable/Reference.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int a=5;
+    int& b=a;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    a++;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    b++;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<&a<<endl;
+    cout<<&b<<endl;
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
