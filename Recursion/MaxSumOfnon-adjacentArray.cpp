0cbacd4f8e5d705792f51282a66da20c6b787977
Arpit Rai <arpitr2002@outlook.com>
2024-05-07 03:36:26 +0530

    recursion

---
+++ Recursion/MaxSumOfnon-adjacentArray.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<limits.h>
+using namespace std;
+
+void sum(vector<int>& a,int s,int& n,int i,int& maxi){
+    if(i>=n){
+        maxi=max(maxi,s);
+        return;
+    }
+
+    //exclude
+    sum(a,s,n,i+1,maxi);
+
+    //include
+    s+=a[i];
+    sum(a,s,n,i+2,maxi);
+
+
+
+
+}
+ 
+int main(){
+    vector<int> a{2,4,13,9};
+    int n=a.size();
+    int s=0;
+    int i=0;
+    int maxi=INT_MIN;
+    sum(a,s,n,i,maxi);
+    cout<<maxi;
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
