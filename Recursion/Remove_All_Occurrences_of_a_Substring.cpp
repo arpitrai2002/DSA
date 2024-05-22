f9363f459535c8c602015f3994e03fc42f1d8954
Arpit Rai <arpitr2002@outlook.com>
2024-05-22 23:46:36 +0530

    r

---
+++ Recursion/Remove_All_Occurrences_of_a_Substring.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void findstring(string& a,string& b){
+    int f=a.find(b);
+    if(f!=string::npos){
+        string left=a.substr(0,f);
+        string right=a.substr(f+b.size(),a.size());
+        a=left+right;
+        findstring(a,b);
+    }
+    else{
+        return;
+    }
+}
+
+string fs(string a,string b){
+    int find=a.find(b);
+    int len=b.length();
+    while(find!=string::npos){
+        a.erase(find,len);
+        find=a.find(b);
+    }
+    return a;
+}
+ 
+int main(){
+    string a="babcaabcbcdababcc";
+    string part="abc";
+    cout<<fs(a,part)<<endl;
+    findstring(a,part);
+    cout<<a;
+
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
