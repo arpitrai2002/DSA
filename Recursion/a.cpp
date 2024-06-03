e14c6b8c1b62318d23ddbb64285cba0013eb4fcc
Arpit Rai <arpitr2002@outlook.com>
2024-06-04 01:03:52 +0530

    r

--- Recursion/a.cpp
+++ Recursion/a.cpp
@@ -1,10 +1,40 @@
 #include<iostream>
 using namespace std;
+
+bool magicNumber(string& a,int ai,string& b,int bi){
+    if(a.size()==ai && b.size()==bi){
+        return true;
+    }
+
+    if(a.size()==ai && b.size()>bi){
+        while(b.size()>bi){
+            if(b[bi]!='*'){
+                return false;
+            }
+            bi++;
+        }
+        return true;
+    }
+    if(a[ai]==b[bi] || '?'==b[bi]){
+        return magicNumber(a,ai+1,b,bi+1);
+    }
+
+    if(b[bi]=='*'){
+        bool caseA=magicNumber(a,ai+1,b,bi);
+        bool caseB=magicNumber(a,ai,b,bi+1);
+
+        return caseA || caseB;
+    }
+    return false;
+}
  
 int main(){
- 
- 
- 
+    string a="abcdef";
+    string b="ab??ef";
+    int ai=0;
+    int bi=0;
+    cout<<magicNumber(a,ai,b,bi);
+
  
  
  return 0;
