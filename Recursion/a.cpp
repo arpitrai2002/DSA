711c557e1d73bd281d50a619a98eed5f4c8020e5
Arpit Rai <arpitr2002@outlook.com>
2024-06-06 23:57:39 +0530

    r

--- Recursion/a.cpp
+++ Recursion/a.cpp
@@ -30,7 +30,7 @@ bool magicNumber(string& a,int ai,string& b,int bi){
  
 int main(){
     string a="abcdef";
-    string b="ab??ihef";
+    string b="aef";
     int ai=0;
     int bi=0;
     cout<<magicNumber(a,ai,b,bi);
