953591675d035cf136459c79d03a393bd479b65a
Arpit Rai <arpitr2002@outlook.com>
2024-06-08 03:05:10 +0530

    r

--- Recursion/MinimumCostForTickets.cpp
+++ Recursion/MinimumCostForTickets.cpp
@@ -27,7 +27,7 @@ int minCost(vector<int>& days,vector<int>& costs,int i){
 }
 
 int main(){
-    vector<int> days={1,2,3,4,5,6,7,8,9,10,30,31};
+    vector<int> days={1,4,6,7,8,20};
     vector<int> costs={2,7,15};
     cout<<minCost(days,costs,0);
 
