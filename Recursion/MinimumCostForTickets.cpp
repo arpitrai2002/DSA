71925efc5af5aae6f1852565ce50143cd7a9e501
Arpit Rai <arpitr2002@outlook.com>
2024-06-08 00:04:27 +0530

    r

---
+++ Recursion/MinimumCostForTickets.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int minCost(vector<int>& days,vector<int>& costs,int i){
+    if(i>=days.size()){
+        return 0;
+    }
+
+    int cost1=costs[0]+minCost(days,costs,i+1);
+
+    int passValid=days[i]+7-1;
+    int j=i;
+    while(j<days.size() && days[j]<=passValid){
+        j++;
+    }
+    int cost7=costs[1]+minCost(days,costs,j);
+
+    passValid=days[i]+7-1;
+    j=i;
+    while(j<days.size() && days[j]<=passValid){
+        j++;
+    }
+    int cost30=costs[2]+minCost(days,costs,j);
+
+    return min(cost1,min(cost7,cost30));
+}
+
+int main(){
+    vector<int> days={1,2,3,4,5,6,7,8,9,10,30,31};
+    vector<int> costs={2,7,15};
+    cout<<minCost(days,costs,0);
+
+ return 0;
+}
